7c6ca9aabb27f3f464966e6667db16eed2320260
santoshinawkhare <gunjannawkhare1@gmail.com>
2021-12-01 12:46:00 +0530

    Add project files.

---
+++ Day_27_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_27_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
